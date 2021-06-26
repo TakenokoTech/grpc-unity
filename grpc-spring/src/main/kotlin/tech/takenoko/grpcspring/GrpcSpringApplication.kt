e8cb5bf5f189c15ea148d5709055c2ce0a6d1ecc
takenoko <takenoko.tech@gmail.com>
2021-06-26 14:31:04 +0900

    first commit.

---
+++ grpc-spring/src/main/kotlin/tech/takenoko/grpcspring/GrpcSpringApplication.kt
@@ -0,0 +1,11 @@
+package tech.takenoko.grpcspring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GrpcSpringApplication
+
+fun main(args: Array<String>) {
+	runApplication<GrpcSpringApplication>(*args)
+}
